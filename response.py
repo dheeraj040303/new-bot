54c650bc1829faa69c10990496632246b8247816
unknown <dheerajdalbanjan0403@gmail.com>
2023-01-24 22:44:24 +0530

    asdf

---
+++ response.py
@@ -0,0 +1,13 @@
+
+def sample_response(input_text):
+    text = input_text.lower()
+    if text in ("hi", "hello"):
+        return "Hi there this is linkerIn bot"
+    elif text in ("who are you", "who are you?"):
+        return "I'am a bot that all about movie"
+    elif text.split(" ")[0] == '/convert':
+        try:
+            return
+        except Exception as error:
+            return error
+    return
